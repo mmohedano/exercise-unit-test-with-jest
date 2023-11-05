9a50cd16e2b32e103d55e21fd1413c4c77dc179f
Marta Mohedano <139046036+mmohedano@users.noreply.github.com>
2023-11-05 09:48:19 +0000

    test

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
-const fromEuroToDollar = function(valueInEuro) {
-    // Convertimos el valor a dólares
-    let valueInDollar = valueInEuro * 1.07;
-    // Retornamos el valor en dólares
-    return valueInDollar;
-}
-const fromDollarToYen = function(valueInEuro) {
-    // Convertimos el valor a dólares
-    let valueInYen = (valueInEuro * 1.07)*156.5;
-    // Retornamos el valor en dólares
-    return valueInYen;
-}
-/*const fromYenToPound = function(valueInEuro) {
-    // Convertimos el valor a dólares
-    let valueInPound = ((valueInEuro * 1.07)*156.5)*0.87;
-    // Retornamos el valor en dólares
-    return valueInPound;
-}
-*/
 
-const sum = (a, b) => {
-    return a+b;
-}
-console.log(sum(4,6))
+const squareSum = (numbers) => {
+    let sum = 0
+    numbers.forEach( number => {
+        sum += (Math.pow (number,2))
+    });
+    return sum;
+  }
+ 
 
-module.exports = {sum, fromEuroToDollar};
+
+module.exports = {squareSum};
--- test.js
+++ test.js
@@ -1,32 +1,6 @@
-const {sum} = require('./app.js')
-test ('adds 14 + 9 to equal 23', ()=> {
-    let total = sum (14, 9);
-    expect(total).toBe(23);
-});
 
-test("One euro should be 1.07 dollars", function() {
-    // Importo la funcion desde app.js
-    const { fromEuroToDollar } = require('./app.js');
 
-    // Uso la función como debe ser usada
-    const dollars = fromEuroToDollar(3.5);
-
-    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
-    const expected = 3.5 * 1.07;
-
-    // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
-})
-test("One dollar should be 167.455 yens", function() {
-    // Importo la funcion desde app.js
-    const { fromDollarToYen } = require('./app.js');
-
-    // Uso la función como debe ser usada
-    const yens = fromDollarToYen(3.5);
-
-    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
-    const expected = (3.5 * 1.07)*156.5;
-
-    // Hago mi comparación (la prueba)
-    expect(fromDollarToYen(3.5)).toBe(586.0925); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
-})
+const {squareSum} = require('./app.js')
+test ('sum squares numbers 3, 4, 2 to equal 29', () => {
+    let total = squareSum([3, 4, 2]);
+    expect(total).toEqual(29);
